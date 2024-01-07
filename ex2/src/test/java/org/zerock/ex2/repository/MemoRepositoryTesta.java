d63ce731cb92984a49dad470adeb8fa877b41e94
ZZZZZZZIWON <pjw20120436@gmail.com>
2024-01-07 19:40:10 +0900

    studying thymeleaf

--- ex2/src/test/java/org/zerock/ex2/repository/MemoRepositoryTesta.java
+++ ex2/src/test/java/org/zerock/ex2/repository/MemoRepositoryTesta.java
@@ -1,14 +1,18 @@
 package org.zerock.ex2.repository;
 
+import jakarta.persistence.Column;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 import org.zerock.ex2.entity.Memo;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
@@ -89,8 +93,54 @@ class MemoRepositoryTests {
         System.out.println("Total Count: " + result.getTotalElements());
         System.out.println("Page Number: " + result.getNumber());
         System.out.println("Page Size: " + result.getSize());
-        System.out.println("has Nex PAge?: " + result.hasNext());
+        System.out.println("has Next Page?: " + result.hasNext());
         System.out.println("first page?: " + result.isFirst());
+
+        System.out.println("=================================");
+
+        for (Memo memo : result.getContent()) {
+            System.out.println("memo = " + memo);
+        }
+    }
+
+    @Test
+    public void testSort() {
+        Sort sort1 = Sort.by("mno").descending();
+        Sort sort2 = Sort.by("memoText").ascending();
+        Sort sortAll = sort1.and(sort2);
+
+        Pageable pageable  = PageRequest.of(0, 10, sortAll);
+
+        Page<Memo> result = memoRepository.findAll(pageable);
+
+        result.get().forEach(memo -> {
+            System.out.println("memo = " + memo);
+        });
+    }
+
+    @Test
+    public void testQueryMethods() {
+        List<Memo> list = memoRepository.findByMnoBetweenOrderByMnoDesc(70L, 80L);
+
+        for (Memo memo : list) {
+            System.out.println("memo = " + memo);
+        }
+    }
+
+    @Test
+    public void testQueryMethodWithPageable() {
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("mno").descending());
+
+        Page<Memo> result = memoRepository.findByMnoBetween(10L, 50L, pageable);
+        result.get().forEach(memo -> System.out.println("memo = " + memo));
+    }
+
+
+    @Commit
+    @Transactional
+    @Test
+    public void testDeleteQueryMethods() {
+        memoRepository.deleteMemoByMnoLessThan(10L);
     }
 
 
